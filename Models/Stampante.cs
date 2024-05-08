afc6c0abf14df7b587dfe05d31f0b6393fecaa95
Maurizio Conti <mconti@users.noreply.github.com>
2024-05-08 17:57:42 +0200

    Prima versione

---
+++ Models/Stampante.cs
@@ -0,0 +1,8 @@
+public class Stampante 
+{
+    public int Id { get; set;}
+    public string? Modello { get; set;}
+    public string? Nome { get; set;}
+    public DateTime DataAcquisto { get; set;}
+
+}
