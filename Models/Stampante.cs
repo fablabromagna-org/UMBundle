5da0c01085f5aca30bdb0c0b8be866d46c865407
Maurizio Conti <mconti@users.noreply.github.com>
2024-05-08 18:30:58 +0200

    .

--- Models/Stampante.cs
+++ Models/Stampante.cs
@@ -1,8 +1,9 @@
 public class Stampante 
 {
-    public int Id { get; set;}
-    public string? Modello { get; set;}
     public string? Nome { get; set;}
+    public string? Immagine { get; set;}
+    public int? Ore { get; set;}
     public DateTime DataAcquisto { get; set;}
-
+    public string strDataAcquisto => DataAcquisto.ToString("d MMMM yyyy", new System.Globalization.CultureInfo("it-IT"));
+    public string? Pdf { get; set;}
 }
